dca3f6a9b2167a185409bebcb65903d44ac96cb3
Luka <odak.skywalker@gmail.com>
2020-02-25 09:55:28 +0100

    update lambdas

--- exams/Person.java
+++ exams/Person.java
@@ -96,44 +96,64 @@ public class Person{
 				&& p.getAge() >= 18
 				&& p.getAge() <= 25
 		);
+		
+	public static void processPersons(List<Person> roster, Predicate<Person> tester, Consumer<Person> block){
+		for(Person p: roster){
+			if(tester.test(p)){
+				block.accept(p);
+			}
+		}
+	}
 	
+	processPersons(
+		roster,
+		p -> p.getGender() == Person.Sex.MALE
+			&& p.getAge() >= 18
+			&& p.getAge() <= 25,
+		p -> p.printPerson()
+	);
 	
+	public static void processPersonsWithFunction(List<Person> roster, Predicate<Person> tester,
+													Function<Person, String> mapper, Consumer<String> block){
+		for(Person p: roster){
+			if(tester.test(p)){
+				String data = mapper.apply(p);
+				block.accept(data);
+			}
+		}
+	}
 	
+	processPersonsWithFunction(
+		roster,
+		p -> p.getGender() == Person.Sex.MALE
+			&& p.getAge() >= 18
+			&& p.getAge() <= 25,
+		p -> p.getEmailAddreess(),
+		email -> System.out.println(email)
+	);
 	
+	public static <X, Y> void processElements(
+		Iterable<X> source,
+		Predicate<X> tester,
+		Function <X, Y> mapper,
+		Consumer<Y> block) {
+			for(X p: source){
+				if(tester.test(p)){
+					Y data = mapper.apply(p);
+					block.accept(data);
+				}
+			}
+		}
+	)
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+	roster
+		.stream()
+		.filter(p -> p.getGender() == Person.Sex.MALE
+					&& p.getAge() >= 18
+					&& p.getAge() <= 25)
+		.map(p -> p.getEmailAddreess())
+		.forEach(email -> System.out.println(email));
+
+
+
 }
